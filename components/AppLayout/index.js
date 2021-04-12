de8eccbb4e5e4bc71a6f8298d175fe8506825790
Daniel Díaz <69810853+danield413@users.noreply.github.com>
2021-04-12 12:22:24 -0500

    Primera clase

---
+++ components/AppLayout/index.js
@@ -0,0 +1,35 @@
+
+import React from 'react'
+
+export const AppLayout = ({children}) => {
+    return (
+        <>
+            <main>
+                {children}
+            </main>
+            <style jsx global>{`
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+                Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+            }
+            
+            a {
+              color: inherit;
+              text-decoration: none;
+            }
+            
+            * {
+              box-sizing: border-box;
+            }
+
+            h1{
+                color: red;
+            }
+            
+            `}</style>
+        </>
+    )
+}
---
+++ pages/timeline/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import { AppLayout } from "../../components/AppLayout"
+
+const Timeline = ({ username }) => {
+    return (
+        <>
+            <AppLayout>
+                <h1>timeline of {username}</h1>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </AppLayout>
+
+            {/* <style jsx>{`
+                h1 {
+                    font-size: 20px;
+                    color: red;
+                }
+            `}</style> */}
+        </>
+    )
+}
+
+export default Timeline
+
+Timeline.getInitialProps = () => {
+    return fetch('http://localhost:3000/api/hello')
+            .then( res => res.json() )
+            .then( response => {
+                console.log(response);
+                const { username } = response;
+                return {username};
+            }) 
+}
